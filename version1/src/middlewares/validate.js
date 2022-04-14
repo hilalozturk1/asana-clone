006240728d416f3fcb108bf9a11b45ddd7f0eb64
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-14 21:42:08 +0300

    add a middleware to manage any route

---
+++ version1/src/middlewares/validate.js
@@ -0,0 +1,17 @@
+const httpStatus = require("http-status");
+
+const validate = (schema) => (req, res, next) => {
+    const { value, error } = schema.validate(req.body);
+    if(error){
+        //error.details = [{message : ""}, {message : ""}]
+         const errorMessage = error.details?.map((detail) => detail.message).join(", ");
+        //["","",""]  =>  "a, b, c"
+        res.status(httpStatus.BAD_REQUEST).json({error : errorMessage});
+        return;
+    }
+
+    Object.assign(req, value);
+    return next();
+};
+
+module.exports = validate;
