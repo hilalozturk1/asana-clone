2ac9558ec882ad35badc85592a8af8cd556672ee
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-14 21:55:17 +0300

    middleware works on routes

--- version1/src/api-routes/Projects.js
+++ version1/src/api-routes/Projects.js
@@ -1,11 +1,15 @@
 //validations desc.
+const schemas = require("../validations/Projects");// get schemas
 //validate middleware
+const validate = require("../middlewares/validate");//get func
 const express = require("express");
 const { create, index } = require("../controllers/Projects")
 const router = express.Router();
 
 router.get("/", index)
-router.post("/", create)
+router
+.route("/")
+.post(validate(schemas.createValidation), create);
 
 module.exports = {
     router,
