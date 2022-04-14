9c01d0c78f2cb048a5645ebc9e380f897089db8e
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-14 12:04:41 +0300

    connection between services and controllers & http-status package

--- version1/src/controllers/Projects.js
+++ version1/src/controllers/Projects.js
@@ -1,8 +1,13 @@
 const { insert } = require("../services/Projects");
-
+const httpStatus = require("http-status");
 const create = (req, res) => {
-    insert({ name:"hilal", lname:"ozturk"})
-    res.status(200).send("Project Create");
+    insert({ name:"hilal"})
+    .then((response) => {
+        res.status(httpStatus.CREATED).send(response);//201
+    })
+    .catch((e) => {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e);
+    })
 };
 
 const index = (req, res) => {//to get responses to all projects
--- version1/src/services/Projects.js
+++ version1/src/services/Projects.js
@@ -1,6 +1,8 @@
 //service layer usually performs registration on model-
+const Project = require("../models/Projects")
 const insert = (projectData) => {
-    console.log(projectData)
+    const projects = new Project(projectData);
+    return projects.save();
 };
 
 module.exports = {
