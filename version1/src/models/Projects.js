6ec91ae6cc21b3d4ad36c4fcf4e5873907e5e536
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-20 19:05:35 +0300

    pre and post hooks

--- version1/src/models/Projects.js
+++ version1/src/models/Projects.js
@@ -10,4 +10,13 @@ const ProjectSchema = new Mongoose.Schema({
     timestamps : true , versionKey : false
 });
 
+// ProjectSchema.pre("save", (next, doc) => {//model hooks
+//     console.log('PRE', doc)
+//     next();
+// })
+
+ProjectSchema.post("save", (object) => {
+    console.log("POST", object);//logging
+})
+
 module.exports = Mongoose.model("project", ProjectSchema)
