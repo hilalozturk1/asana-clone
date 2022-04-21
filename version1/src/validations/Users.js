2cc6f122a4fb9f0b8669180ea746153cdc3bbc3a
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-21 16:21:16 +0300

    loginValidation

--- version1/src/validations/Users.js
+++ version1/src/validations/Users.js
@@ -6,6 +6,12 @@ const createValidation = Joi.object({
     email : Joi.string().email().required().min(8),
 });
 
+const loginValidation = Joi.object({
+    password : Joi.string().required().min(8),
+    email : Joi.string().email().required().min(8),
+});
+
 module.exports = {
-    createValidation
+    createValidation,
+    loginValidation
 }
