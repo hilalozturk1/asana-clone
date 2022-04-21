497113bea8788aaf3c0ced112aa997ccc1df4b6d
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-21 16:08:12 +0300

    add route to login

--- version1/src/api-routes/Users.js
+++ version1/src/api-routes/Users.js
@@ -1,11 +1,12 @@
 const schemas = require("../validations/Users");
 const validate = require("../middlewares/validate");
 const express = require("express");
-const { create, index } = require("../controllers/Users");
+const { create, index, login } = require("../controllers/Users");
 
 const router = express.Router();
 
 router.get("/", index);
 router.route("/").post(validate(schemas.createValidation), create);
+router.route("/login").post(validate(schemas.loginValidation), login);
 
 module.exports = router;
