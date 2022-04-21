7d0169e2fd84eac7a4e633c2840a6d13fa4edb5a
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-21 13:57:29 +0300

    add email validation

--- version1/src/validations/Users.js
+++ version1/src/validations/Users.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const createValidation = Joi.object({
     full_name : Joi.string().required().min(3),
     password : Joi.string().required().min(8),
-    email : Joi.string().required().min(8),
+    email : Joi.string().email().required().min(8),
 });
 
 module.exports = {
