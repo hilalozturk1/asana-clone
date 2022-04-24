aa0226f76a969c66c194206ba01d8ecfd92def0e
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-24 22:50:48 +0300

    get new password

--- version1/src/api-routes/Users.js
+++ version1/src/api-routes/Users.js
@@ -2,7 +2,7 @@ const schemas = require("../validations/Users");
 const validate = require("../middlewares/validate");
 const authenticate = require("../middlewares/authenticate");
 const express = require("express");
-const { create, index, login, projectsList } = require("../controllers/Users");
+const { create, index, login, projectsList, resetPassword } = require("../controllers/Users");
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.get("/", index);
 router.route("/").post(validate(schemas.createValidation), create);
 router.route("/login").post(validate(schemas.loginValidation), login);
 router.route("/projects").get(authenticate,projectsList)
+router.route("/reset-password").post(validate(schemas.resetPasswordValidation), resetPassword)
 
 module.exports = router;
