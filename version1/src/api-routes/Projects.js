a1ceab4ca6fc6ac228e8790c694ca9d6e6fa427f
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-25 15:20:18 +0300

    delete project

--- version1/src/api-routes/Projects.js
+++ version1/src/api-routes/Projects.js
@@ -3,12 +3,13 @@ const schemas = require("../validations/Projects");// get schemas
 //validate middleware
 const validate = require("../middlewares/validate");//get func
 const express = require("express");
-const { create, index, update } = require("../controllers/Projects");
+const { create, index, update, deleteProject } = require("../controllers/Projects");
 const authenticate = require("../middlewares/authenticate");
 const router = express.Router();
 
 router.route("/").get(authenticate, index);
 router.route("/").post(authenticate ,validate(schemas.createValidation), create);
+router.route("/:id").delete(authenticate, deleteProject)
 router.route("/:id").patch(authenticate, validate(schemas.updateValidation), update)
 
 module.exports = {
--- version1/src/controllers/Projects.js
+++ version1/src/controllers/Projects.js
@@ -1,4 +1,4 @@
-const { insert, list, modify } = require("../services/Projects");
+const { insert, list, modify, remove } = require("../services/Projects");
 const httpStatus = require("http-status");
 const create = (req, res) => {
     req.body.user_id = req.user;
@@ -27,8 +27,28 @@ const update = (req, res) => {
         res.status(httpStatus.OK).send(updatedProject)
     }).catch((e) => { res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "couldn't be registered" }) })
 }
+
+const deleteProject = (req, res) => {
+    if(!req.params.id) {
+        return res.status(httpStatus.BAD_REQUEST).send({
+            message : "haven't find ID info"
+        });
+    }
+    remove(req.params?.id).then((deletedProject) => {
+        if(!deletedProject){
+            return res.status(httpStatus.NOT_FOUND).send({
+                message: "no such project was found"
+            })
+        }
+        res.status(httpStatus.OK).send({
+            message: "the project has been deleted"
+        })
+    }).catch((e) => { res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "a problem occurred during deletion process" }) })
+}
+
  module.exports = {
      create,
      index,
-     update
+     update,
+     deleteProject
  }
