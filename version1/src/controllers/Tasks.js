50b8cd42c50be2fc4a18062a790d8593bc970b9c
hilalozturk1 <hlloztrk68@gmail.com>
2022-10-11 15:06:40 +0300

    added controllers for tasks

---
+++ version1/src/controllers/Tasks.js
@@ -0,0 +1,55 @@
+const { insert, list, modify, remove } = require("../services/Tasks");
+const httpStatus = require("http-status");
+const create = (req, res) => {
+    req.body.user_id = req.user;
+    insert(req.body)
+    .then((response) => {
+        res.status(httpStatus.CREATED).send(response);//201
+    })
+    .catch((e) => {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e);
+    })
+};
+
+const index = (req, res) => {//to get responses to all projects
+    if(!req?.params?.projectId) return res.status(httpStatus.BAD_REQUEST).send({ error : "project information is missing.."})
+    list( {project_id : req.params.projectId } ).then((response) => {
+        res.status(httpStatus.OK).send(response);
+    }).catch((e) => { res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e) });
+};
+
+const update = (req, res) => {
+    if(!req.params.id) {
+        return res.status(httpStatus.BAD_REQUEST).send({
+            message : "haven't find ID info"
+        });
+    }
+    modify(req.body, req.params?.id).then((updatedDoc) => {
+        res.status(httpStatus.OK).send(updatedDoc)
+    }).catch((e) => { res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "couldn't be registered" }) })
+}
+
+const deleteTask = (req, res) => {
+    if(!req.params.id) {
+        return res.status(httpStatus.BAD_REQUEST).send({
+            message : "haven't find ID info"
+        });
+    }
+    remove(req.params?.id).then((deletedDoc) => {
+        if(!deletedDoc){
+            return res.status(httpStatus.NOT_FOUND).send({
+                message: "no such task was found"
+            })
+        }
+        res.status(httpStatus.OK).send({
+            message: "the task has been deleted"
+        })
+    }).catch((e) => { res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error : "a problem occurred during deletion process" }) })
+}
+
+ module.exports = {
+     create,
+     index,
+     update,
+     deleteTask
+ }
