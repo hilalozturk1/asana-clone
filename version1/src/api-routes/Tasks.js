00e1189792abcf1092d1f75587a6ad1ffabf224e
hilalozturk1 <hlloztrk68@gmail.com>
2022-10-13 21:45:40 +0300

    added endpoint to make comment

--- version1/src/api-routes/Tasks.js
+++ version1/src/api-routes/Tasks.js
@@ -3,13 +3,14 @@ const schemas = require("../validations/Tasks");// get schemas
 //validate middleware
 const validate = require("../middlewares/validate");//get func
 const express = require("express");
-const { create, update, deleteTask } = require("../controllers/Tasks");
+const { create, update, deleteTask, makeComment } = require("../controllers/Tasks");
 const authenticate = require("../middlewares/authenticate");
 const router = express.Router();
 
 router.route("/").post(authenticate ,validate(schemas.createValidation), create);
 router.route("/:id").delete(authenticate, deleteTask)
 router.route("/:id").patch(authenticate, validate(schemas.updateValidation), update)
+router.route("/:id/make-comment").post(authenticate, validate(schemas.commentValidation), makeComment)
 
 module.exports = {
     router,
