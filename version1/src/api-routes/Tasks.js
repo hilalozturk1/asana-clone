025beabca684a8f52feb6d8d87506f2e676181df
hilalozturk1 <hlloztrk68@gmail.com>
2022-10-17 21:37:43 +0300

    added fetch task endpoint

--- version1/src/api-routes/Tasks.js
+++ version1/src/api-routes/Tasks.js
@@ -3,10 +3,11 @@ const schemas = require("../validations/Tasks");// get schemas
 //validate middleware
 const validate = require("../middlewares/validate");//get func
 const express = require("express");
-const { create, update, deleteTask, makeComment, deleteComment, addSubTask } = require("../controllers/Tasks");
+const { create, update, deleteTask, makeComment, deleteComment, addSubTask, fetchTask } = require("../controllers/Tasks");
 const authenticate = require("../middlewares/authenticate");
 const router = express.Router();
 
+router.route("/:id").get(authenticate, fetchTask)
 router.route("/").post(authenticate ,validate(schemas.createValidation), create);
 router.route("/:id").delete(authenticate, deleteTask)
 router.route("/:id").patch(authenticate, validate(schemas.updateValidation), update)
